db11c7598af0677d657b5a7e59247569d7427539
Desktop.Admin <desktop.admin@ceteqdev.local>
2026-03-06 13:30:35 +0100

    New Tasks & Task components

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,12 @@
-import { Component, EventEmitter, Input, Output, output} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrls: ['./tasks.component.css'],
+  imports: [TaskComponent]
 })
 export class TasksComponent {
 
